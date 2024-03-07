e1d6a70bb8050a8c462652cada711c6684f30191
Alisson Honório <alisson.silva@qwasolucoes.com.br>
2024-03-07 15:41:37 -0300

    commit

---
+++ xadrez/src/boardgame/Position.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Position {
+
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int colum) {
+		this.row = row;
+		this.column = colum;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int colum) {
+		this.column = colum;
+	}
+	
+	@Override
+	public String toString() {
+		return row+","+column;
+		
+	}
+}
